a74a8e07ceefb31c969cfa243a72cb780a49be9f
Yoga Bayu AP <yogabayusbi@gmail.com>
2024-06-02 01:50:19 +0700

    update

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -30,78 +30,14 @@
             <v-overlay opacity=".06" scrim="primary" contained model-value persistent />
           </v-card>
         </v-col>
-        <!-- 
-        <v-col cols="6">
-          <v-card
-            append-icon="mdi-open-in-new"
-            class="py-4"
-            color="surface-variant"
-            href="https://vuetifyjs.com/introduction/why-vuetify/#feature-guides"
-            prepend-icon="mdi-star-circle-outline"
-            rel="noopener noreferrer"
-            rounded="lg"
-            subtitle="Explore available framework Features."
-            target="_blank"
-            title="Features"
-            variant="text"
-          >
-            <v-overlay
-              opacity=".06"
-              scrim="primary"
-              contained
-              model-value
-              persistent
-            />
+        <v-col cols="12" md="6">
+          <v-card class="py-4" color="surface-variant" prepend-icon="fas fa-question" rel="noopener noreferrer" rounded="lg"
+            text="Fitur yang lain masih proses"
+            target="_blank" title="Lainnya" variant="text" >
+            <v-overlay opacity=".06" scrim="primary" contained model-value persistent />
           </v-card>
         </v-col>
-
-        <v-col cols="6">
-          <v-card
-            append-icon="mdi-open-in-new"
-            class="py-4"
-            color="surface-variant"
-            href="https://vuetifyjs.com/components/all"
-            prepend-icon="mdi-widgets-outline"
-            rel="noopener noreferrer"
-            rounded="lg"
-            subtitle="Discover components in the API Explorer."
-            target="_blank"
-            title="Components"
-            variant="text"
-          >
-            <v-overlay
-              opacity=".06"
-              scrim="primary"
-              contained
-              model-value
-              persistent
-            />
-          </v-card>
-        </v-col>
-
-        <v-col cols="6">
-          <v-card
-            append-icon="mdi-open-in-new"
-            class="py-4"
-            color="surface-variant"
-            href="https://discord.vuetifyjs.com"
-            prepend-icon="mdi-account-group-outline"
-            rel="noopener noreferrer"
-            rounded="lg"
-            subtitle="Connect with Vuetify developers."
-            target="_blank"
-            title="Community"
-            variant="text"
-          >
-            <v-overlay
-              opacity=".06"
-              scrim="primary"
-              contained
-              model-value
-              persistent
-            />
-          </v-card>
-        </v-col> -->
+       
       </v-row>
     </v-responsive>
   </v-container>
@@ -121,5 +57,9 @@ function toggleTheme() {
 
 function showalert() {
   router.push('/package');
+}
+
+function onprogress() {
+  alert("fitur lain masih proses, hubungi via intagram @yogabayuap");
 }
 </script>
